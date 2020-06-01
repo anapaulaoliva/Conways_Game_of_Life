c97bd9b5119abb817bfe55dcdad44a531c781f23
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-01 11:51:53 -0500

    Updates

---
+++ precache-manifest.9e566b5159e7121dcb7eb23b06650f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157c940211da47fbaf213e3a030e3f67",
+    "url": "/Conways_Game_of_Life/index.html"
+  },
+  {
+    "revision": "fb1e618d143cad8e02c9",
+    "url": "/Conways_Game_of_Life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/Conways_Game_of_Life/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Conways_Game_of_Life/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1e618d143cad8e02c9",
+    "url": "/Conways_Game_of_Life/static/js/main.24df9291.chunk.js"
+  },
+  {
+    "revision": "052cf49e5a1c9e258a9a",
+    "url": "/Conways_Game_of_Life/static/js/runtime-main.17a38695.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Conways_Game_of_Life/static/media/logo.5d5d9eef.svg"
+  }
+]);
