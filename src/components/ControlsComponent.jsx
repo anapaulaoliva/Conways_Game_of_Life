bc68bc74bf0f5dfeef8907502b7f8641d9ae6817
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-01 20:38:01 -0500

    :pencil: Writing docs: Controls component

---
+++ src/components/ControlsComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ControlsStyle from '../styles/Controls.module.css';
+
+const Controls = () => {
+    return (
+        <section>
+            <button>play</button>
+            <button>random</button>
+            <button>clear</button>
+        </section>
+    );
+};
+
+export default Controls;
--- src/components/GameComponent.jsx
+++ src/components/GameComponent.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Board from './BoardComponent';
+import Controls from './ControlsComponent';
 import '../App.css';
 
 const Game = () => {
     return (
         <>
         <div className="App">
+            <Controls/>
             <Board/>
         </div>
         </>
