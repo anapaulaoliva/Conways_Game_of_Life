eccfdc13c89b4c7e96bcb754cd74bcfef5467306
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-01 22:37:24 -0500

    :construction: Work in progress: Board: setting state of play button

--- src/components/BoardComponent.jsx
+++ src/components/BoardComponent.jsx
@@ -3,8 +3,8 @@ import produce from 'immer';
 import BoardStyle from '../styles/Board.module.css';
 
 const Board = () => {
-    const numRows = 20;
-    const numCols = 20;
+    const numRows = 30;
+    const numCols = 30;
 
     const [grid, setGrid] = useState(() => {
         const rows = [];
@@ -15,30 +15,38 @@ const Board = () => {
         return rows;
     });
 
-    console.log(grid);
+    const [running, setRunning] = useState(false);
 
     return (
-        <main style={{
-            gridTemplateColumns: `repeat(${numCols}, 15px)`
-        }}>
-            {grid.map((rows, i) =>
-                rows.map((col, j) => (
-                    <div
-                    className={BoardStyle.cell}
-                    key={`${i}-${j}`} 
-                    onClick={() => {
-                        const newGrid = produce(grid, gridCopy => {
-                            gridCopy[i][j] = 1;
-                        });
-                        setGrid(newGrid);
-                    }}
-                    style={{
-                        backgroundColor: grid[i][j] ? 'lavenderblush' : undefined
-                    }} 
-                    />
-                ))
-            )}
-        </main>
+        <>
+            <button onClick={()=>{
+                setRunning(!running);
+                }}
+            >
+            {running ? 'stop' : 'start'}
+            </button>
+            <main style={{
+                gridTemplateColumns: `repeat(${numCols}, 15px)`
+            }}>
+                {grid.map((rows, i) =>
+                    rows.map((col, j) => (
+                        <div
+                        className={BoardStyle.cell}
+                        key={`${i}-${j}`} 
+                        onClick={() => {
+                            const newGrid = produce(grid, gridCopy => {
+                                gridCopy[i][j] = 1;
+                            });
+                            setGrid(newGrid);
+                        }}
+                        style={{
+                            backgroundColor: grid[i][j] ? 'lavenderblush' : undefined
+                        }} 
+                        />
+                    ))
+                )}
+            </main>
+        </>
     );
 };
 
