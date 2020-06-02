959befa5d1237bb382ce230e3b94d21bc0e12d9f
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-01 21:30:54 -0500

    :clown_face: Mocking things (Bidimensional array size for the grid of the board)

--- src/components/BoardComponent.jsx
+++ src/components/BoardComponent.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BoardStyle from '../styles/Board.module.css';
 
 const Board = () => {
+    const numRows = 10;
+    const numCols = 10;
+
+    const [grid, setGrid] = useState(() => {
+        const rows = [];
+        for (let i = 0; i < numRows; i++) {
+            rows.push(Array.from(Array(numCols), () => 0))
+        }
+
+        return rows;
+    });
+
+    console.log(grid);
+
     return (
-            <main>Board with squares</main>
+
+        <main>Board with squares</main>
     );
 };
 
