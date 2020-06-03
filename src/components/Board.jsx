4fe417adb5687f9260f33e391fab3ad4d0db9a8e
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-02 23:22:12 -0500

    :fire: Removing deadlines of code

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -27,7 +27,7 @@ const Board = () => {
         return Array(numRows).fill(Array(numCols).fill(0));
     };
     
-    const [grid, setGrid] = useState( ()=> {;
+    const [grid, setGrid] = useState( ()=> {
         return generateEmptyGrid();
     });
 
@@ -37,18 +37,14 @@ const Board = () => {
     runningRef.current = running;
 
     const runSimulation = useCallback(() => {
-        setGeneration(counter)
-        counter++;
-        console.log(`corresimulacion time: ${counter}`);
-        
-        /*TODO:return !runningRef.current ? null
-            :*/
+        setGeneration(counter);
+        counter++;        
         if (!runningRef.current) {
             return;
         }
-        /*update state in g: current value of the grid
-        gridCopy expected to conserve original grid*/
+        //update state of g: current value of the grid
         setGrid((g) => {
+            //gridCopy expected to conserve original grid
             return produce(g, gridCopy => {
                 //for loops go through every single cell of the grid
                 for (let i = 0; i < numRows; i++) {
@@ -60,37 +56,12 @@ const Board = () => {
                             const newI = i + x;
                             const newJ = j + y;
                             //conditionals to delimitate checking above or below the grid values
-
-                            /*TODO:switch(newI, newJ) {
-                                case (newI > 0 && newI < numRows 
-                                    && newJ >= 0 && newJ < numCols):
-                                    neighbors+= g[newI][newJ]
-                                    break;
-                            }*/
-
-                            /*TODO:neighbors+= g[newI][newJ] ?
-                                newI > 0 &&
-                                newI < numRows &&
-                                newJ >= 0 &&
-                                newJ < numCols*/
-                                
                             if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
                                     //counter of how many neighbors the cell has
-                                    neighbors += g[newI][newJ]
+                                    neighbors += g[newI][newJ];
                                 }
                         })
                         //Conditional for the death of a cell
-                        /*TODO:switch(neighbors) {
-                            case (neighbors < 2 || neighbors > 3):
-                                gridCopy[i][j] = 0;
-                                break;
-                            case(g[i][j] === 0 && neighbors === 3):
-                                gridCopy[i][j] = 1;
-                                break;
-                        }*/
-                        /*TODO:return (neighbors < 1 || neighbors > 3) ? gridCopy[i][j] = 0 
-                        : (g[i][j] === 0 && neighbors === 3) ? gridCopy[i][j] = 1;*/
-
                         if (neighbors < 2 || neighbors > 3) {
                             //current position dies.
                             gridCopy[i][j] = 0;
@@ -109,13 +80,9 @@ const Board = () => {
 
     const simulateButton = () => {
         setRunning(!running);
-        /*checking through button's state to switch between true or false
-        given the actual state -ref- of the button*/
+        /*checking playbtn state given the actual state -ref- of the button*/
         switch(!running) {
-            case (!running):
-                runningRef.current = true;
-                runSimulation();
-                break;
+            case (!running): runningRef.current = true; runSimulation(); break;
         }
     };
 
@@ -144,11 +111,10 @@ const Board = () => {
                 }}
             />
 
-            <main style={{
-                gridTemplateColumns: `repeat(${numCols}, 15px)`
-            }}>
+            <main style={{ gridTemplateColumns: `repeat(${numCols}, 15px)`}}>
                 {grid.map((rows, i) =>
                     rows.map((col, j) => (
+                        //rendering each cell square
                         <div
                             className={BoardStyle.Cell}
                             key={`${i}-${j}`} 
@@ -163,6 +129,7 @@ const Board = () => {
                     ))
                 )}
             </main>
+            
             <div className={BoardStyle.GenCounter}>
                 <span>g e n</span>
                 <div className={BoardStyle.Gen}>
